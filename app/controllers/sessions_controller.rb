d65fce90b2472cbded3287dbb15ca83af9526dc6
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-16 11:09:56 -0700

    updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,39 @@ class SessionsController < ApplicationController
 
   end
 
-  def login
-    author = Author.find_by(name: params[:username])
+  def create
+    @auth_hash = request.env['omniauth.auth']
+    ap @auth_hash
 
-    if author
-      flash[:success] = "Successfully logged in as #{ author.name }"
-      session[:author_id] = author.id
-      redirect_to root_path
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user_id
+      flash[:success] = "Welcome #{@user.name}"
     else
-      flash[:error] = "No author found by the name #{ params[:username] } "
-      redirect_to root_path
+      @user = User.new uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email']
+
+      if @user.save
+        session[:user_id] = @user_id
+        flash[:success] = "Welcome #{@user.name}"
+      else
+        flash[:error] = "Unable to save user!"
+      end
     end
+    redirect_to root_path
   end
+
+def login
+  author = Author.find_by(name: params[:username])
+
+  if author
+    flash[:success] = "Successfully logged in as #{ author.name }"
+    session[:author_id] = author.id
+    redirect_to root_path
+  else
+    flash[:error] = "No author found by the name #{ params[:username] } "
+    redirect_to root_path
+  end
+end
 
 end
